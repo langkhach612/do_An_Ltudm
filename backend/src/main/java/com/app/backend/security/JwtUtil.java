e2d50aa41e8acc546c3af96f9aeee7dfd7e24806
langkhach612 <khoi0089567@huce.edu.vn>
2025-04-08 16:55:25 +0700

    add_security_8/4

---
+++ backend/src/main/java/com/app/backend/security/JwtUtil.java
@@ -0,0 +1,54 @@
+package com.app.backend.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+    // Khóa bí mật, bạn nên để trong file cấu hình
+    private final String SECRET_KEY = "my_super_secret_key_12345678901234567890"; // >= 32 ký tự
+
+    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 10; // 10 giờ
+
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
